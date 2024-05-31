99afc005d4a53617412dc1c7ee8d055501de3495
denery <rorynette@gmail.com>
2024-05-31 12:52:36 +0300

    Initial

---
+++ src/main/java/moe/denery/reconcept/ReConcept.java
@@ -0,0 +1,13 @@
+package moe.denery.reconcept;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReConcept implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/moe/denery/reconcept/client/ReConceptClient.java
@@ -0,0 +1,13 @@
+package moe.denery.reconcept.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReConceptClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
